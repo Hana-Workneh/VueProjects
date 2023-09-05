ce2e878ef0d55fb84089dc836ab22129e15fd585
Hana-Workneh <hanandarge19@gmail.com>
2023-09-05 13:54:16 +0300

    re routing using button

--- RouterVueProj/src/views/CarView.vue
+++ RouterVueProj/src/views/CarView.vue
@@ -1,8 +1,17 @@
 <script setup>
-    import { useRoute, RouterView } from 'vue-router';
+    import { useRoute, useRouter, RouterView } from 'vue-router';
     import cars from '../data/cars.json'
     import ContactView from './ContactView.vue'    
+    /* 
+    DIFFERENCE BETWEEN useRoute AND useRouter:
+    useRoute is a composible that allows us to extract info about the current path that we are in but we cannot adjust our path
+
+    useRouter is also a composible which allows us adjust/ update our path
+    */
     const route = useRoute()
+    const router = useRouter()
+
+    const carId = parseInt(route.params.id)
 
     const car = cars.find(c => c.id === parseInt (route.params.id))
 
@@ -14,6 +23,7 @@
         <p>{{car.name}}</p>
         <p>{{car.year}}</p>
         <p>{{car.price}}</p>
+        <button @click="router.push(`/cars/${carId}/contact`)">Click for Contact</button>
         <RouterView /> 
     </div>
 </template>
